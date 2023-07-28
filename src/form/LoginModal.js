7cde445dadca59264608efc8829f29e3e5add9b7
Truc Tran <tracytrle@gmail.com>
2023-07-27 22:57:57 -0500

    fix loginform

--- src/form/LoginModal.js
+++ src/form/LoginModal.js
@@ -8,7 +8,7 @@ const style = {
   flexWrap: "wrap",
   display: "flex",
   justifyContent: "center",
-  overflow: "scroll",
+  overflow: "hidden",
   position: "absolute",
   top: "50%",
   left: "50%",
